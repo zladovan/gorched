8c387aa823d6518f6a2277b9223f6f397571a38e
zladovan <cabala.jan@gmail.com>
2020-06-04 10:51:54 +0200

    refactor: ger rid out of utils
    Last method in utlis was moved to debug.

--- debug.go
+++ debug.go
@@ -101,3 +101,14 @@ func (v *debugView) Tick(e tl.Event) {
 		v.hidden = !v.hidden
 	}
 }
+
+// DrawPallette draws all available colors as column with 6 colors per line.
+// Given shift will change starting color and will result in printing less than 256 colors.
+// Debug only !
+func DrawPallette(s *tl.Screen, shift int) {
+	for c := 0; c < 256-shift; c++ {
+		x := c % 6
+		y := c / 6
+		s.RenderCell(x, y, &tl.Cell{Fg: tl.Attr(c + shift), Ch: '█'})
+	}
+}
